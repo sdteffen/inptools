efbec4cafc7434e070d70fed7e0562a0eaad6659
Steffen Macke <sdteffen@sdteffen.de>
2011-09-24 10:30:15 +0200

    New program to show the about dialog.

---
+++ src/inptools-about.c
@@ -0,0 +1,27 @@
+/**
+ * inptools-about.c Display Inptools about dialog
+ * 
+ * (c) 2011 Steffen Macke <sdteffen@sdteffen.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#include <windows.h>
+
+int main(int argc, char *argv[])
+{
+	MessageBox(NULL, "Inptools 0.2.3\nCopyright (c) 2008 - 2011 Steffen Macke\nPortions Copyright (c) 1999, 2000 Frank Wamerdam\n\nhttp://epanet.de/inptools", "Inptools", MB_OK);
+	return 0;
+}
